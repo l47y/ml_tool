f825b00cc3a7e1adb50f61d5b92629769458fb32
Unknown <nicolaskeller92@googlemail.com>
2018-11-08 17:36:15 +0100

    add some images

--- experiments.R
+++
@@ -1,7 +0,0 @@
-titanic = read_csv('/home/nicolas/Escritorio/RProjects/ml_tool/data/titanic.csv')
-titanic %<>% delete_colsWithManyFactors() %>% createDummyFeatures() %>% make_strToFactors()
-wherenas = apply(titanic, 1, function(row) {any(is.na(row))})
-titanic = titanic[!wherenas, ]
-task = makeClassifTask(id = 'task', titanic, target = 'Survived')
-learner = makeLearner('classif.logreg', par.vals = list())
-model = train(task = task, learner = learner)
